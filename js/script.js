f496aebb153f411efc29def91003e92a3edd84c5
Mateusz Król <m_krol@poczta.wwsi.edu.pl>
2017-01-31 14:18:24 +0100

    Add second project

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*var age = prompt('Podaj swój wiek');
+if (age < 18) {
+    alert('Nie jesteś pełnooletni');
+} else {
+    alert('Możesz wejść:)');
+}
+*/
+var name = prompt('Wpisz swoje imię');
+alert('Witaj,' + name);
+//alert('testowe powiadomienie dla'+ name);
+console.log('Witaj,' + name);
+//console.log('Masz: ' + age);
